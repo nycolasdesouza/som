425a6a90e71fb4e66c0eb5ba30daab74fb6210ea
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-10-20 10:50:59 -0300

    gbnbg

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-//declaraçao/criaçaoda funçao tocasompom
+//declaraçao/criaçaoda funçao tocaSomPom
 function tocaSomPom(){    
     document.querySelector('#som_tecla_pom').play();
 }
 //chamada ou invocaçao a funçao tocasompom ao clicar no botao
-document.querySelector('.tecla_pom').onclik = tocasomclap
+document.querySelector('.tecla_pom').onclick = tocaSomPom
