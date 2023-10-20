1f6128d2631f26cf4f8cde6d77170ad86eda939f
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-10-20 10:56:38 -0300

    vr

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.querySelector('.tecla_pom').onclick = tocaSomPom
 
 //declaraçao/criaçaoda funçao tocaSomClap
 function tocaSomClap(){    
-    document.querySelector('#som_tecla_pom').play();
+    document.querySelector('#som_tecla_Clap').play();
 }
 //chamada ou invocaçao a funçao tocaSomClap ao clicar no botao
 document.querySelector('.tecla_Clap').onclick = tocaSomClap
