cb426d9350066aec18543b211d281914f1124387
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-10-20 10:55:50 -0300

    fv

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function tocaSomPom(){
 }
 //chamada ou invocaçao a funçao tocasompom ao clicar no botao
 document.querySelector('.tecla_pom').onclick = tocaSomPom
+
+//declaraçao/criaçaoda funçao tocaSomClap
+function tocaSomClap(){    
+    document.querySelector('#som_tecla_pom').play();
+}
+//chamada ou invocaçao a funçao tocaSomClap ao clicar no botao
+document.querySelector('.tecla_Clap').onclick = tocaSomClap
