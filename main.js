76a181ce2aa4118416d7e3802de3d315ff23aa8f
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-11-10 10:52:42 -0300

    yguyuuy

--- main.js
+++ main.js
@@ -1,62 +1,17 @@
-//declaraçao/criaçaoda funçao tocaSomPom
-function tocaSomPom(){    
-    document.querySelector('#som_tecla_pom').play();
+//Declaração/criação da função tocaSomCade
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-//chamada ou invocaçao a funçao tocasompom ao clicar no botao
-document.querySelector('.tecla_pom').onclick = tocaSomPom
-
-//declaraçao/criaçaoda funçao tocaSomClap
-function tocaSomClap(){    
-    document.querySelector('#som_tecla_clap').play();
+//criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+while(contador < listaDeTeclas.length){
+    const  teclas = listaDeTeclas[contador]
+    const efeito = teclas.classList[1];
+    const idAudio = "#som_" + efeito;
+    teclas.onclick = function(){
+        tocaSom(idAudio)
+    }
+    contador = contador + 1;
+    console.log(contador);
 }
-//chamada ou invocaçao a funçao tocaSomClap ao clicar no botao
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-//declaraçao/criaçaoda funçao tocaSomTim
-function tocaSomTim(){    
-    document.querySelector('#som_tecla_tim').play();
-}
-//chamada ou invocaçao a funçao tocaSomTim ao clicar no botao
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-//declaraçao/criaçaoda funçao tocaSomPuff
-function tocaSomPuff(){    
-    document.querySelector('#som_tecla_puff').play();
-}
-//chamada ou invocaçao a funçao tocaSomPuff ao clicar no botao
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-//declaraçao/criaçaoda funçao tocaSomSplash
-function tocaSomSplash(){    
-    document.querySelector('#som_tecla_splash').play();
-}
-//chamada ou invocaçao a funçao tocaSomSplash ao clicar no botao
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-//declaraçao/criaçaoda funçao tocaSomToim
-function tocaSomToim(){    
-    document.querySelector('#som_tecla_toim').play();
-}
-//chamada ou invocaçao a funçao tocaSomToim ao clicar no botao
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-//declaraçao/criaçaoda funçao tocaSomPsh
-function tocaSomPsh(){    
-    document.querySelector('#som_tecla_psh').play();
-}
-//chamada ou invocaçao a funçao tocaSomPsh ao clicar no botao
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-//declaraçao/criaçaoda funçao tocaSomTic
-function tocaSomTic(){    
-    document.querySelector('#som_tecla_tic').play();
-}
-//chamada ou invocaçao a funçao tocaSomTic ao clicar no botao
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-//declaraçao/criaçaoda funçao tocaSomTom
-function tocaSomTom(){    
-    document.querySelector('#som_tecla_tom').play();
-}
-//chamada ou invocaçao a funçao tocaSomTic ao clicar no botao
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
