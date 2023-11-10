c7401212665cbae1a503e84be64a2048e796f4c7
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-11-10 11:09:08 -0300

    kjgk

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
+
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
+//criação da repetição "while" e trocado pelo "for" com variavel, condição e incremento
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
-    const  teclas = listaDeTeclas[contador];
-    const efeito = teclas.classList[1];
-    const idAudio = `#som_${efeito}`;
-    teclas.onclick = function(){
-        tocaSom(idAudio);
+    const  teclas = listaDeTeclas[contador]; //guarda o cantador da lista 
+    const efeito = teclas.classList[1]; //chama o 2º item da classe individual do som
+    const idAudio = `#som_${efeito}`; //utiliza a junção template string ${}
+    teclas.onclick = function(){ //função anônima auxiliar para chamar tocaSom
+        tocaSom(idAudio); //chamada da função tocaSom com parâmetro idAudio
     }
 }
