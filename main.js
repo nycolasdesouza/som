e57d82313a23e1428bd620bd674de316f30dea9e
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-11-17 11:18:58 -0300

    rr

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 //criação da repetição "while" e trocado pelo "for" com variavel, condição e incremento
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
-    const  teclas = listaDeTeclas[contador]; //guarda o cantador da lista 
+    const teclas = listaDeTeclas[contador]; //guarda o cantador da lista 
     const efeito = teclas.classList[1]; //chama o 2º item da classe individual do som
     const idAudio = `#som_${efeito}`; //utiliza a junção template string ${}
     teclas.onclick = function(){ //função anônima auxiliar para chamar tocaSom
         tocaSom(idAudio); //chamada da função tocaSom com parâmetro idAudio
     }
     teclas.onkeydown = function(evento){
-        if(evento.code!= 'tab'){
+        if(evento.code != 'Tab'){
        teclas.classList.add('ativa');
     }
 }
-    teclas.onkeydown = function(){
+    teclas.onkeyup = function(){
         teclas.classList.remove('ativa');
     }
 }
