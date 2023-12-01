01a1f071b4b929859b62937bb6027293610d4369
nycolasdesouza <105080906+nycolasdesouza@users.noreply.github.com>
2023-12-01 11:12:58 -0300

    fgngfjjg

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-//Declaração/criação da função tocaSomCade
-function tocaSom(idElementoAudio) {
+//Declaração/criação da função tocaSom
+function tocaSom (idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-//criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-//criação da repetição "while" e trocado pelo "for" com variavel, condição e incremento
+
 for(let contador = 0; contador < listaDeTeclas.length; contador++){
-    const teclas = listaDeTeclas[contador]; //guarda o cantador da lista 
-    const efeito = teclas.classList[1]; //chama o 2º item da classe individual do som
-    const idAudio = `#som_${efeito}`; //utiliza a junção template string ${}
-    teclas.onclick = function(){ //função anônima auxiliar para chamar tocaSom
-        tocaSom(idAudio); //chamada da função tocaSom com parâmetro idAudio
+    const teclas = listaDeTeclas[contador] 
+    const efeito = teclas.classList[1];
+    const idAudio = `#som_${efeito}`;  
+    teclas.onclick = function(){
+    tocaSom(idAudio); 
     }
     teclas.onkeydown = function(evento){
-
-        console.log(evento.code)
-
-        if(evento.code ==='space') {
-            teclas.classList.add('ativa');
+        if(evento.code != 'Tab'){
+    teclas.classList.add('ativa');
+        }
     }
-}
     teclas.onkeyup = function(){
         teclas.classList.remove('ativa');
     }
 }
+
